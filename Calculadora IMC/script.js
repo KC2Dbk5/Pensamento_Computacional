a1d7633576d4f243da257c3c58986e1354d90e63
Kayran Henrique Reis Teixeira (KC2Dbk5) <147454100+KC2Dbk5@users.noreply.github.com>
2024-05-05 18:38:39 -0300

    Add files via upload

---
+++ Calculadora IMC/script.js
@@ -0,0 +1,4 @@
+var altura = parseFloat(document.getElementById('altura').value);
+var peso = parseFloat(document.getElementById('peso').value);
+
+var imc = (document.getElementById('resultado')).peso / (altura * altura);
