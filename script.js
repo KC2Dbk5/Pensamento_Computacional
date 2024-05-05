93d87570651295c36a2c3680fa916a69545c63b8
Kayran Henrique Reis Teixeira (KC2Dbk5) <147454100+KC2Dbk5@users.noreply.github.com>
2024-05-05 18:39:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-var altura = parseFloat(document.getElementById('altura').value);
-var peso = parseFloat(document.getElementById('peso').value);
-
-var imc = (document.getElementById('resultado')).peso / (altura * altura);
