640464060396241c51daf441780be97114bf8829
Kayran Henrique Reis Teixeira (KC2Dbk5) <147454100+KC2Dbk5@users.noreply.github.com>
2024-05-13 19:51:26 -0300

    Update script.js

--- Calculadora IMC/script.js
+++ Calculadora IMC/script.js
@@ -1,4 +1,13 @@
-var altura = parseFloat(document.getElementById('altura').value);
-var peso = parseFloat(document.getElementById('peso').value);
+function calcularIMC() {
+    const altura = parseFloat(document.getElementById('altura').value);
+    const peso = parseFloat(document.getElementById('peso').value);
 
-var imc = (document.getElementById('resultado')).peso / (altura * altura);
+    if (altura > 0 && peso > 0) {
+        const imc = peso / (altura * altura);
+        const resultado = `Seu IMC é: ${imc.toFixed(2)}`;
+
+        document.getElementById('resultado').innerHTML = resultado;
+    } else {
+        document.getElementById('resultado').innerHTML = "Preencha altura e peso válidos.";
+    }
+}
